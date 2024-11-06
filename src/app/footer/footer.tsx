d73563b64b5249432613fc46c6d7836f1ff2cd03
Cole <coleblvck@gmail.com>
2024-11-06 14:14:22 +0100

    Minor UX and visual tweak.

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -4,9 +4,10 @@ import { info } from "../config/about";
 export default function Footer() {
     return (
         <footer>
-            <span>&copy; {new Date().getFullYear()}, <a className="footer-link" href={info.footerLink} target="_blank">{info.name}</a>.</span>
-            <span> Designed by <a className="footer-link" id="designer-attribution" href="https://dayo.codes" target="_blank">dayo.codes</a>.
-            </span>    
+            <div>&copy; {new Date().getFullYear()}, <a className="footer-link" href={info.footerLink} target="_blank">{info.name}</a>.
+            </div>
+            <div>Designed by <a className="footer-link" id="designer-attribution" href="https://dayo.codes" target="_blank">dayo.codes</a>.
+            </div>    
         </footer>
     )
 }
